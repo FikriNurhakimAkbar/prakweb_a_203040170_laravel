90180b712c67c8d4e956a5b4db31144bf82e1bb3
FikriNurhakimAkbar <nurhakimf2906@example.com>
2022-10-26 22:04:00 +0700

    Menyelesaikan Factory & Faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
   <h2>{{$post->title}}</h2>
-  <p>By. <a href="#" class="text-decoration-none">{{$post->user->name}}</a> in <a class="text-decoration-none" in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
+  <p>By. <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> in <a class="text-decoration-none" in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
   {!! $post->body !!}
 
   <a class="d-block mt-5" href="/blog">Back To Posts</a>
