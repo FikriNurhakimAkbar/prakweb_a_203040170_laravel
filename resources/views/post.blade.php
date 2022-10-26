857f746abb1253cda2d45df36d9982acd508ee3c
FikriNurhakimAkbar <nurhakimf2906@example.com>
2022-10-26 22:24:11 +0700

    Menyelesaikan Redisign UI

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.main')
 
 @section('container')
-  <h2>{{$post->title}}</h2>
-  <p>By. <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> in <a class="text-decoration-none" in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
-  {!! $post->body !!}
+<div class="row justify-content-center mb-5">
+  <div class="col-md-8">
+    <h1 class="mb-3">{{$post->title}}</h1>
+    <p>By. <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
 
-  <a class="d-block mt-5" href="/blog">Back To Posts</a>
+    <img src="https://source.unsplash.com/1200x400?{{$post->category->name}}" alt="{{$post->category->name}}" class="img-fluid">
+
+    <article class="my-3 fs-5">
+      {!! $post->body !!}
+    </article>
+
+    <a class="d-block mt-5" href="/blog">Back To Posts</a>
+  </div>
+</div>
 @endsection
