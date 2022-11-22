661c678c8a3f55cf6176b6948c271fa0e99421fc
FikriNurhakimAkbar <nurhakimf2906@example.com>
2022-11-22 15:58:15 +0700

    Menyelesaikan Update & Delete Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+
 
 class DashboardPostController extends Controller
 {
@@ -42,19 +43,18 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-           'title' => 'required|max:255',
-           'slug' => 'required|unique:posts',
-           'category_id' => 'required',
-           'body' =>'required'
+        $validateData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
         ]);
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
-        $validatedData['user_id'] = auth()->user()->id;
-        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+        Post::create($validateData);
 
-        Post::create($validatedData);
-
-return redirect('/dashboard/posts')->with('success', 'New post has been added!');
+        return redirect('/dashboard/posts')->with('success', 'New Post has been added!');
     }
 
     /**
@@ -63,10 +63,67 @@ return redirect('/dashboard/posts')->with('success', 'New post has been added!')
      * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
      */
+    public function show(Post $post)
+    {
+        return view('dashboard.posts.show', [
+            'post' => $post
+        ]);
+    }
+     /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $rules = ([
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'slug' => 'required|unique:posts',
+            'body' => 'required'
+        ]);
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+        $validateData = $request->validate($rules);
+
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)
+            ->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
 
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
 
     public function checkSlug(Request $request)
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,7 +8,11 @@
 
         <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span>Back to all my posts</a>
         <a href="" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
-        <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>Delete</a>
+        <form action="/dashboard/posts/{{$post->slug}}"method="post" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="btn btn-danger" onclick="return confirm('are you sure')"><span data-feather="x-circle"></span>Delete</button>
+          </form>
 
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" alt="{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
@@ -16,8 +20,6 @@
         <article class="my-3 fs-5">
           {!! $post->body !!}
         </article>
-
-
 
       </div>
     </div>
